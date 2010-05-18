1dc40ad9f1a8eb40404d0cae284c6f180f7968e1
Karl Jonathan Ward <karl.j.ward@googlemail.com>
2010-05-18 11:49:05 +0100

    Initial commit.

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageNode;
+
+
+public class Main {
+	
+	public static void parsePdf(File f) throws IOException {
+		PDDocument doc = PDDocument.load(f);
+		PDDocumentCatalog docCat = doc.getDocumentCatalog();
+		
+		PDPageNode root = docCat.getPages();
+		List pages = new ArrayList();
+		root.getAllKids(pages);
+		
+		TextExtractor te = new TextExtractor();
+		
+		for (Object p : pages) {
+			PDPage page = (PDPage) p;
+			te.processStream(page, 
+							 page.getResources(), 
+							 page.getContents().getStream());
+		}
+		
+		doc.close();
+		
+		System.out.println(te.toString());
+	}
+	
+	public static void main(String[] filenames) {
+		for (String filename : filenames) {
+			try {
+				parsePdf(new File(filename));
+			} catch (IOException e) {
+				Logger.getAnonymousLogger().log(Level.WARNING,
+												e.toString());
+			}
+		}
+	}
+
+}
---
+++ src/TextExtractor.java
@@ -0,0 +1,142 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.graphics.PDGraphicsState;
+import org.apache.pdfbox.pdmodel.graphics.color.PDColorState;
+import org.apache.pdfbox.util.PDFStreamEngine;
+import org.apache.pdfbox.util.TextPosition;
+
+
+public class TextExtractor extends PDFStreamEngine {
+	
+	private ArrayList<TextRun> textRuns = new ArrayList<TextRun>();
+
+	protected void showCharacter(TextPosition tp) {
+		PDGraphicsState gs = getGraphicsState();
+		
+		// try to find an existing text run that fits
+		boolean added = false;
+		
+		for (TextRun tr : textRuns) {
+			if (tr.hasMatchingStyle(tp, gs)) {
+				if (tr.isIncidentToLeft(tp)) {
+					tr.addBefore(tp.getCharacter());
+					added = true;
+					break;
+				} else if (tr.isIncidentToRight(tp)) {
+					tr.addAfter(tp.getCharacter());
+					added = true;
+					break;
+				}
+			}
+		}
+		
+		if (!added) {
+			textRuns.add(TextRun.newFor(tp, gs));
+		}
+	}
+	
+	public String toString() {
+		String s = "";
+		for (TextRun tr : textRuns) {
+			s += tr.run + " @ " + tr.x + "," + tr.y 
+						+ " : " + tr.font.getBaseFont()
+						+ " C " + tr.strokeColor.toString()
+						+ "\n";
+		}
+		return s;
+	}
+}
+
+class TextRun {
+	float x;
+	float y;
+	float width;
+	String run;
+	PDFont font;
+	PDColorState strokeColor;
+	PDColorState nonStrokeColor;
+	
+	static TextRun newFor(TextPosition tp, PDGraphicsState gs) {
+		TextRun tr = new TextRun();
+		tr.x = tp.getX();
+		tr.y = tp.getY();
+		tr.font = tp.getFont();
+		tr.strokeColor = gs.getStrokingColor();
+		tr.nonStrokeColor = gs.getNonStrokingColor();
+		tr.run = tp.getCharacter();
+		
+		try {
+			tr.width = tr.font.getStringWidth(tr.run);
+		} catch (IOException e) {
+			Logger.getAnonymousLogger()
+			      .log(Level.WARNING, "Can't get string width for new TextRun.");
+		}
+		 
+		return tr;
+	}
+	
+	private static float looseness(PDFont font) {
+		try {
+			float spaceWidth = font.getStringWidth(" ");
+			return spaceWidth * 2;
+		} catch (IOException e) {
+			Logger.getAnonymousLogger()
+				  .log(Level.WARNING, "Can't get font space width.");
+			return 0;
+		}
+	}
+	
+	TextRun addBefore(String s) {
+		run = s + run;
+		
+		try {
+			width = font.getStringWidth(run);
+		} catch (IOException e) {
+			Logger.getAnonymousLogger()
+			      .log(Level.WARNING, "Can't update TextRun width.");
+		}
+		
+		return this;
+	}
+	
+	TextRun addAfter(String s) {
+		run += s;
+		
+		try {
+			width = font.getStringWidth(run);
+		} catch (IOException e) {
+			Logger.getAnonymousLogger()
+				  .log(Level.WARNING, "Can't update TextRun width.");
+		}
+		
+		return this;
+	}
+	
+	boolean isIncidentToLeft(TextPosition tp) {
+		final float mostAcceptableLeft = x - looseness(font);
+		final float mostAcceptableRight = x;
+		
+		return y == tp.getY() 
+				&& tp.getX() >= mostAcceptableLeft
+				&& tp.getX() <= mostAcceptableRight;
+	}
+	
+	boolean isIncidentToRight(TextPosition tp) {
+		final float mostAcceptableLeft = x + width;
+		final float mostAcceptableRight = x + width + looseness(font);
+		
+		return y == tp.getY()
+				&& tp.getX() >= mostAcceptableLeft
+				&& tp.getY() <= mostAcceptableRight;
+	}
+	
+	boolean hasMatchingStyle(TextPosition tp, PDGraphicsState gs) {
+		return tp.getFont() == font
+				&& gs.getStrokingColor() == strokeColor
+				&& gs.getNonStrokingColor() == nonStrokeColor;
+	}
+}
